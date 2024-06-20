1113ce9e3a585804a9ce5fd0f00ce7d0b408b8d5
noear <noear@live.cn>
2024-06-20 17:35:05 +0800

    2.8.4

---
+++ solon-cloud-event/README.md
@@ -0,0 +1,15 @@
+
+消息事务支持：
+
+* activemq
+* folkmq
+* rabbitmq
+* rocketmq5（只能1条）
+* kafka
+
+消息实例级订阅支持（默认为集群级）：
+
+* water
+* folkmq
+* rabbitmq
+* rocketmq5
---
+++ solon-cloud-event/rocketmq-solon-cloud-plugin/src/main/java/org/noear/solon/cloud/extend/rocketmq/RocketmqProps.java
@@ -0,0 +1,11 @@
+package org.noear.solon.cloud.extend.rocketmq;
+
+import org.noear.solon.cloud.CloudProps;
+
+/**
+ * @author noear
+ * @since 1.2
+ */
+public class RocketmqProps {
+    public static final String GROUP_SPLIT_MARK = "--";
+}

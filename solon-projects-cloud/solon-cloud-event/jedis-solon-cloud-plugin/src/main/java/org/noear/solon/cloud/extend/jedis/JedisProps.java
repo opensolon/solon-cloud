9a097c32cdf3b1d3cae398d02be985f1050a9194
noear <noear@live.cn>
2024-06-20 16:41:22 +0800

    2.8.4

---
+++ solon-projects-cloud/solon-cloud-event/jedis-solon-cloud-plugin/src/main/java/org/noear/solon/cloud/extend/jedis/JedisProps.java
@@ -0,0 +1,9 @@
+package org.noear.solon.cloud.extend.jedis;
+
+/**
+ * @author noear
+ * @since 1.10
+ */
+public class JedisProps {
+    public static final String GROUP_SPLIT_MARK = ":";
+}
---
+++ solon-projects-cloud/solon-cloud/local-solon-cloud-plugin/src/main/java/org/noear/solon/cloud/extend/local/LocalProps.java
@@ -0,0 +1,9 @@
+package org.noear.solon.cloud.extend.local;
+
+/**
+ * @author noear
+ * @since 1.11
+ */
+public class LocalProps {
+    public static final String GROUP_TOPIC_SPLIT_MART = ":";
+}

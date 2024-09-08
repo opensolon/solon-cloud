7a3c6864278c5975563022ede55c800239fdcacf
noear <noear@live.cn>
2024-09-08 19:14:56 +0800

    CloudJob 构建启用新的分类构建机投制

--- solon-cloud-job/xxl-job-solon-cloud-plugin/src/main/java/org/noear/solon/cloud/extend/xxljob/XPluginImp.java
+++ solon-cloud-job/xxl-job-solon-cloud-plugin/src/main/java/org/noear/solon/cloud/extend/xxljob/XPluginImp.java
@@ -22,8 +22,8 @@ import org.noear.solon.Solon;
 import org.noear.solon.Utils;
 import org.noear.solon.cloud.CloudManager;
 import org.noear.solon.cloud.CloudProps;
+import org.noear.solon.cloud.annotation.CloudJob;
 import org.noear.solon.cloud.extend.xxljob.service.CloudJobServiceImpl;
-import org.noear.solon.cloud.impl.CloudJobBeanBuilder;
 import org.noear.solon.core.AppContext;
 import org.noear.solon.core.BeanWrap;
 import org.noear.solon.core.Plugin;
@@ -55,8 +55,8 @@ public class XPluginImp implements Plugin {
         //注册Job服务
         CloudManager.register(new CloudJobServiceImpl());
 
-        //添加 @CloudJob 对 IJobHandler 类的支持 //@since 2.0
-        CloudJobBeanBuilder.getInstance().addBuilder(IJobHandler.class,(clz, bw, anno) -> {
+        //添加 @CloudJob 对 IJobHandler 类的支持 //@since 2.0 //@since 2.9
+        context.beanBuilderAdd(CloudJob.class, IJobHandler.class, (clz, bw, anno) -> {
             //支持${xxx}配置
             String name = Solon.cfg().getByTmpl(Utils.annoAlias(anno.value(), anno.name()));
             //提示：不支持CloudJob拦截器

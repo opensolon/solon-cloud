7a7c33f48f55347dd3213cc88f5dbe73ff029513
noear <noear@live.cn>
2025-09-04 09:01:08 +0800

    调整 aliyun-ons-solon-cloud-plugin 插件名

--- solon-cloud-event/aliyun-ons-solon-cloud-plugin/src/main/java/org/noear/solon/cloud/extend/aliyun/ons/AliyunOnsCloudPlugin.java
+++ solon-cloud-event/aliyun-ons-solon-cloud-plugin/src/main/java/org/noear/solon/cloud/extend/aliyun/ons/AliyunOnsCloudPlugin.java
@@ -27,7 +27,7 @@ import org.noear.solon.core.Plugin;
  * @author cgy
  * @since 1.11
  */
-public class XPluginImp implements Plugin {
+public class AliyunOnsCloudPlugin implements Plugin {
     @Override
     public void start(AppContext context) {
         CloudProps cloudProps = new CloudProps(context,"aliyun.ons");

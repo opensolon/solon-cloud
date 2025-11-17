5a50a9ea27a4aef2e54baf2e34d1701eb8e6d196
noear <noear@live.cn>
2025-11-17 17:13:14 +0800

    添加 solon-cloud-gateway 示例

---
+++ solon-cloud/solon-cloud-gateway/src/test/java/demo/ErrorFilter.java
@@ -0,0 +1,31 @@
+package demo;
+
+import org.noear.solon.cloud.gateway.CloudGatewayFilter;
+import org.noear.solon.cloud.gateway.exchange.ExContext;
+import org.noear.solon.cloud.gateway.exchange.ExFilterChain;
+import org.noear.solon.core.exception.StatusException;
+import org.noear.solon.rx.Completable;
+
+/**
+ *
+ * @author noear 2025/11/17 created
+ *
+ */
+public class ErrorFilter implements CloudGatewayFilter {
+    @Override
+    public Completable doFilter(ExContext ctx, ExFilterChain chain) {
+        //ctx.pause();
+
+        return chain.doFilter(ctx).doOnErrorResume(e -> {
+            if (e instanceof StatusException) {
+                StatusException se = (StatusException) e;
+
+                ctx.newResponse().status(se.getCode());
+            } else {
+                ctx.newResponse().status(500);
+            }
+
+            return Completable.complete();
+        });
+    }
+}

0bb775611c7d0b1e4244b85207e8dee20e9a47d0
minghang <757940216@qq.com>
2026-02-11 09:50:45 +0800

    修复查询附件地址bucketName不传时不获取默认配置bucket值报错问题

--- solon-cloud-file/minio-solon-cloud-plugin/src/main/java/org/noear/solon/cloud/extend/minio/service/CloudFileServiceMinioImpl.java
+++ solon-cloud-file/minio-solon-cloud-plugin/src/main/java/org/noear/solon/cloud/extend/minio/service/CloudFileServiceMinioImpl.java
@@ -96,6 +96,9 @@ public class CloudFileServiceMinioImpl implements CloudFileService {
 
     @Override
     public String getTempUrl(String bucket, String key, Duration duration) throws CloudFileException, UnsupportedOperationException {
+        if (Utils.isEmpty(bucket)) {
+            bucket = this.bucketDef;
+        }
 
         long seconds = duration.getSeconds();
 
